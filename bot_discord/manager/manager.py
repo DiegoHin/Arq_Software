46d185d7bbeee1b00379425a86676389768f2d8a
DiegHin <56352789+DiegoHin@users.noreply.github.com>
2021-12-28 18:53:07 -0300

    Add files via upload

---
+++ bot_discord/manager/manager.py
@@ -0,0 +1,79 @@
+from logging import exception
+import os, time, pika, sys
+from typing import Tuple
+from tictactoe import TicTacToe
+
+t = TicTacToe()
+tablero = t.create_board()
+player = 'X' if t.get_random_first_player() == 1 else 'O'
+
+########### CONNEXIÓN A RABBIT MQ #######################
+HOST = os.environ['RABBITMQ_HOST']      
+connection = pika.BlockingConnection(pika.ConnectionParameters(host=HOST,heartbeat=0))
+channel = connection.channel()
+#El consumidor utiliza el exchange 'cartero'
+channel.exchange_declare(exchange='cartero', exchange_type='topic', durable=True)
+#Se crea un cola temporaria exclusiva para este consumidor (búzon de correos)
+result = channel.queue_declare(queue="ttt", exclusive=True, durable=True)
+queue_name = result.method.queue
+#La cola se asigna a un 'exchange'
+channel.queue_bind(exchange='cartero', queue=queue_name, routing_key="ttt")
+
+#####################################################################
+########## ESPERA Y HACE ALGO CUANDO RECIBE UN MENSAJE ##############
+#####################################################################
+
+print(' [*] Waiting for messages. To exit press CTRL+C ')
+
+#consumer
+def callback(ch, method, properties, body):
+        print(body.decode("UTF-8"))
+        arguments = body.decode("UTF-8").split(" ")
+        global player
+        global tablero
+        if (arguments[0] == "!play-again"):
+                a = f"Nuevo tablero generado"
+                channel.basic_publish(exchange='cartero', routing_key="discord_writer", body = a)
+                t.board.clear()
+                tablero = t.create_board()
+                return tablero
+        
+        if (arguments[0] == "!play"):
+                i = int(arguments[1])
+                j = int(arguments[2])
+                
+                if (len(arguments) != 3):
+                        a = f"Jugar como !play <Fila> <Columna>"
+                        channel.basic_publish(exchange='cartero', routing_key="discord_writer", body = a)
+                else:        
+                        print(f"Juega '{player}'; Columna: {i}, Fila: {j}.")
+                        t.fix_spot(i-1, j-1, player)
+        
+                        ########################## MOSTRAR TABLERO EN DISCORD ############################################
+                        lista = t.board[0] + t.board[1] + t.board[2]
+                        line1 = f"----------------Turn of '{player}' player-----------------------\n"
+                        line2 = f"----------------Next turn '{t.swap_player_turn(player)}' player-----------------------\n"
+                        line = f"--------\n"
+                        row1 = f"{str(lista[0])} | {str(lista[1])} | {str(lista[2])}\n"
+                        row2 = f"{str(lista[3])} | {str(lista[4])} | {str(lista[5])}\n"
+                        row3 = f"{str(lista[6])} | {str(lista[7])} | {str(lista[8])}\n"
+                        tablero1 = line1 + row1 + line + row2 + line + row3 + line2
+                        channel.basic_publish(exchange='cartero', routing_key="discord_writer", body = tablero1)
+                        ##################################################################################################
+                        
+                        if t.is_player_win(player):
+                                a = f"Player '{player}' wins the game!\n"
+                                channel.basic_publish(exchange='cartero', routing_key="discord_writer", body = a) #publica el ganador
+                                n = t.board.clear()
+                                return  n #reset tablero?
+                        
+                        if t.is_board_filled():
+                                a = f"Match Draw!\n"
+                                channel.basic_publish(exchange='cartero', routing_key="discord_writer", body = a)
+                                n = t.board.clear()
+                                return  n
+                        
+                        player = t.swap_player_turn(player)
+        
+channel.basic_consume(queue=queue_name, on_message_callback=callback, auto_ack=True)
+channel.start_consuming()
